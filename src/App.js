c0590aca9fad4cac767c5aaa628bd8b4c426dba8
Trung Kiên Nguyễn <kiennt.6795@gmail.com>
2017-07-19 23:06:44 +0700

    weather app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import AppHeader from './Components/app-header.js';
+import AppMain from './Components/app-main.js';
+import AppDisplay from './Components/app-display.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <AppHeader />
+        <AppMain />
+         <AppDisplay /> 
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/app-header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from '../img/icon.png';
+
+class AppHeader extends Component {
+    render() {
+        return (
+            <div className="header">
+                <a href=""><img className="logo" src={logo} alt="logo"/></a>
+                <h1>App Weather</h1>
+            </div>
+        );
+    }
+}
+
+export default AppHeader;
